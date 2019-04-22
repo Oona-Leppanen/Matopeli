7a7ca9808d79b2b4daaa7ffe9dd6330c42b85984
Amy Nymalm <amy.a.nymalm@utu.fi>
2019-04-22 21:41:53 +0300

    Matopeli valmis

---
+++ Snake/src/snake/RenderPanel.java
@@ -0,0 +1,42 @@
+package snake;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class RenderPanel extends JPanel{
+	
+	public static Color matovari = new Color(255, 49, 243);
+	public static Color omenavari = new Color(51, 255, 236);
+	
+	@Override
+	protected void paintComponent(Graphics g) { //Maalataan kaikki (mato, omena, tausta, teksti)
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 400, 340);
+		g.setColor(Color.GRAY);
+		g.drawRect(20, 20, 20*12,20*12);
+		Snake snake = Snake.matopeli;
+		for (Point point : snake.matoosat) { //Maalataan kaikki matoosat
+			g.setColor(matovari);
+			g.fillRect(point.x*20+20, point.y*20+20, 18, 18);
+		}
+		g.setColor(omenavari);
+		if (!snake.peliOhi)//Omena tulee näkyviin kun peli on käynnissä
+			g.fillRect(snake.omena.x*20+20, snake.omena.y*20+20, 18, 18);
+		g.drawString("score: "+snake.score, 290, 100);
+		if (snake.peliOhi && snake.aikayksikko%6!=0 ) {
+		g.drawString("press space", 290, 60);
+		}
+		if (snake.score >=12*12) {
+			g.drawString("You win!", 290, 140);
+		}
+		
+		//System.out.println("yay"); //<-Testi, jotta timeri toimii oikein.
+		
+	}
+	
+}
---
+++ Snake/src/snake/Snake.java
@@ -0,0 +1,139 @@
+package snake;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class Snake implements ActionListener, KeyListener{
+	
+	public JFrame naytto;
+	public static Snake matopeli;
+	public RenderPanel renderPanel;
+	public Timer timer = new Timer(30, this);
+	public ArrayList<Point> matoosat= new ArrayList<Point>(); 
+	public int aikayksikko=0, score =0, hantaPituus;
+	public char direction =DOWN;
+	public Point head;
+	public Point omena;
+	public static final char UP = 'u', DOWN = 'd', LEFT = 'l', RIGHT = 'r';
+	public boolean peliOhi = true;
+	
+	
+	public Snake() {
+		naytto = new JFrame("Matopeli");
+		naytto.setVisible(true);
+		naytto.setSize(400, 340);
+		naytto.setLocation(300,200);;
+		naytto.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		naytto.addKeyListener(this);
+		naytto.add(renderPanel = new RenderPanel());//RenderPanelissa piirretään kaikki
+		head = new Point(5,1); //madon pää
+		omena = new Point(5,8); 
+		timer.start();
+		
+	}
+	
+	public static void main(String[] args) {
+		matopeli = new Snake();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		renderPanel.repaint();
+		aikayksikko++; //Mato ei liiku eteenpäin joka timerin aikayksiköllä, vaan vain joka viidennellä, johon aikayksikkö inttiä tarvitaan
+		
+		if (head.x < 0 || head.x > 11 || head.y < 0 || head.y > 11) {//Jos mato menee rajojen ulkopuolelle
+			peliOhi=true;		
+		}
+		
+		if (aikayksikko%5==0 && head!=null && !peliOhi ) { //Mato liikkuu 0.15 sekunnin välein
+			matoosat.add((Point) head.clone());//Listätään osa matoa
+			if(matoosat.size() > 0) {
+				while(matoosat.size()>hantaPituus+1) { //Poistaa ensimmäisen Pointin, ilman tätä mato kasvaisi koko ajan
+					matoosat.remove(0);
+				}
+			}//Näillä saadaan aikaan madon kasvu oikeaan suuntaan
+			if (direction ==UP)
+				head = new Point(head.x, head.y-1);
+			if (direction == DOWN)
+				head = new Point(head.x, head.y+1);
+			if (direction == LEFT)
+				head = new Point(head.x-1, head.y);
+			if (direction == RIGHT)
+				head = new Point(head.x+1, head.y);
+			//System.out.println(matoosat); //<-näillä saa outprintattuu koko madon ja omenan jos kiinnostaa.
+			//System.out.println(omena);
+			
+			if(omena!=null) {//Omenan myötä häntä kasvaa, ja pisteet lisääntyvät
+				if(head.equals(omena)){
+					score++;
+					hantaPituus++;
+					omena.setLocation(new Random().nextInt(12),new Random().nextInt(12));
+				}//Omenalle uusi koordinaatio, tarkastetaan, ettei omena ilmesty madolle
+				while (onkoMadolla()) {
+					onkoMadolla();	
+				}
+				
+			}
+			for(Point point : matoosat) { //Jos mato osuu itseensä
+				if (point.equals(head)) {
+					peliOhi=true;
+				}
+			}
+		}
+	}
+	
+	public boolean onkoMadolla() { //funktio joka tarkastaa voiko omena tulla esiin aikomassaan paikassa.
+		for (int i=0; i<matoosat.size(); i++) {
+			if (omena.equals(matoosat.get(i))) {
+				omena.setLocation(new Random().nextInt(12),new Random().nextInt(12));								
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {//Tällä ei tee mitään, mutta on pakko olla.
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyPressed(KeyEvent e) { //Saadaan näppäimistö toimimaan
+		int keyCode = e.getKeyCode();
+        if(keyCode == KeyEvent.VK_LEFT) {
+        	direction = LEFT;
+        }
+        if(keyCode == KeyEvent.VK_RIGHT) {
+        	direction = RIGHT;
+        }
+        if(keyCode == KeyEvent.VK_DOWN) {
+        	direction = DOWN;
+        }
+        if(keyCode == KeyEvent.VK_UP) {
+        	direction = UP;
+        }
+        if(peliOhi==true && keyCode == KeyEvent.VK_SPACE){ //Saadaan peli uudelleen käyntiin.
+        	score =0;
+        	direction = DOWN;
+        	matoosat.clear();
+        	hantaPituus=0;
+        	head.move(5, 1);
+        	peliOhi = false; 	
+        }
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {//Tällä ei tee mitää, mutta on pakko olla
+		// TODO Auto-generated method stub
+		
+	}
+
+}
